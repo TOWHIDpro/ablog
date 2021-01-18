25269098c154512adfc3326e9dd07ca2597de6db
TOWHIDpro <sajjad.rk05@gmail.com>
2021-01-18 11:07:06 +0600

    4th commit

--- theblog/models.py
+++ theblog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,3 +12,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+
+
+    def get_absolute_url(self):
+        return reverse('articaldetail', args=(str(self.id)))
+        #return reverse('home')
+        
--- theblog/urls.py
+++ theblog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . views import HomeView, Articalview
+from . views import HomeView, Articalview, Addpostview
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path('artical/<int:pk>', Articalview.as_view(), name="articaldetail")
+    path('artical/<int:pk>', Articalview.as_view(), name="articaldetail"),
+    path('add_post/', Addpostview.as_view(), name="add_post")
 
 
 ]
